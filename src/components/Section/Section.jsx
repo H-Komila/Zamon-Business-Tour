7acd9b24c36ffbace2bf8a07300a494ee9986b86
H - Komila <komilaxibziddinova06@gmail.com>
2026-01-27 11:08:07 +0500

    rostanham tugadi

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -41,7 +41,7 @@ const Section = () => {
     touchThreshold: 20, 
     responsive: [
       { breakpoint: 1280, settings: { slidesToShow: 2 } },
-      { breakpoint: 768, settings: { slidesToShow: 1, dots: true } }
+      { breakpoint: 768, settings: { slidesToShow: 1, dots: false } }
     ]
   };
 
